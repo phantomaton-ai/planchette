f22dd4f25e5f1c620a1bc37c140c361c57fd7e0f
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-01-05 12:32:16 -0800

    prompt

--- plugin.js
+++ plugin.js
@@ -1,1 +1,13 @@
-export default () => ({ install: [] });
+import plugins from 'phantomaton-plugins';
+import system from 'phantomaton-system';
+
+export default plugins.create([
+  system.system.decorator(
+    [],
+    () => (prompt) => () => {
+      const value = prompt();
+      console.log(value);
+      return value;
+    }
+  )
+]);
