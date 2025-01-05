1f58c43991add439d355ebb00718d2ebc425f8a8
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-01-05 12:39:35 -0800

    commands

--- plugin.js
+++ plugin.js
@@ -1,9 +1,26 @@
 import chalk from 'chalk';
 
+import execution from 'phantomaton-execution';
 import plugins from 'phantomaton-plugins';
 import system from 'phantomaton-system';
 
 export default plugins.create([
+  plugins.define(execution.commands).as(() => [{
+    name: 'capitalize',
+    validate: (attributes, body) => !!attributes.text,
+    execute: (attributes) => attributes.text.toUpperCase(),
+    example: { attributes: { text: 'Test' } },
+    desription: 'Capitalizes text'
+  }]),
+
+  plugins.define(execution.commands).as(() => [{
+    name: 'lowercase',
+    validate: (attributes, body) => !!attributes.text,
+    execute: (attributes) => attributes.text.toLowerCase(),
+    example: { attributes: { text: 'Test' } },
+    desription: 'Lower-cases text'
+  }]),
+
   system.system.decorator(
     [],
     () => (prompt) => () => {
