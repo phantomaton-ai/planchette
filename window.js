a969c9cfbab59c122beb097a6022b3316cfd448c
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-03-14 18:15:26 -0700

    window

--- window.js
+++ window.js
@@ -1,3 +1,9 @@
+const validate(index) {
+  if (index < 0) {
+    throw new Exception('Unknown target');
+  }
+}
+
 export default class Window {
   constructor(file, content, options = {}) {
     this.file = file;
@@ -5,33 +11,55 @@ export default class Window {
     this.scroll = options.scroll || 0;
     this.lines = content.split('\n');
     this.size = options.size || 100;
+    this.cursor = 0;
+    this.end = this.cursor;
   }
 
   before(target) {
-    // TODO
+    this.cursor = this.index(target);
   }
   
   after(target) {
-    // TODO
+    this.cursor = this.index(target) + target.length;
   }
   
   select(start, end) {
-    // TODO
+    this.cursor = this.position(this.index(start));
+    this.end = this.position(this.index(end));
   }
   
   drag(target) {
-    // TODO
+    this.cursor = this.index(target) + target.length;
   }
   
   edit(content) {
-    // TODO
+    const before = this.content.slice(0, this.cursor);
+    const after = this.content.slice(this.end);
+    this.content = `${before}${content}${after}`;
+    // TODO save?
   }
   
   scroll(lines) {
     this.scroll = Math.max(0, Math.min(this.lines.length, this.scroll + lines));
   }
 
+  selected() {
+    return this.selecting() ? content.slice(this.cursor, this.end - this.cursor) : '';
+  }
+
+  selecting() {
+    return this.end > this.cursor;
+  }
+
+  find(target) {
+    const index = this.content.indexOf(target);
+    if (index < 0) {
+      throw new Error(`Cannot find ${target}`);
+    }
+    return index;
+  }
+
   view() {
-    // TODO: Show lines frrom scroll to size
+    return this.lines.slice(this.scroll, this.size).join('\n');
   }
 }
